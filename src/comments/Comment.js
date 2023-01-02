85e66f340b90dcdcc820228f9e4bacac24d5d4e0
jothishwar <jothishwarvvr@gmail.com>
2023-01-02 16:09:05 +0530

    rendering single comment

--- src/comments/Comment.js
+++ src/comments/Comment.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
-function Comment() {
+function Comment({key,comment}) {
 	return (
-		<div>
-			<h1>,,,</h1>	
+		<div className="comment">
+			<div className="comment-image-container">
+				<img src="/user-icon.png" alt="usericon" />
+			</div>
+			<div className="comment-right-part">
+				<div className="comment-content">
+					<div className="comment-author">{comment.username}</div>
+					<div>{comment.createdAt}</div>
+				</div>
+				<div className="comment-text">{comment.body}</div>
+			</div>
 		</div>
-	)
-}
+	);
+};
 
 export default Comment

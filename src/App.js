4812da7a7a4954d75eb7e1c4db090b4315120384
jothishwar <jothishwarvvr@gmail.com>
2023-01-02 15:31:17 +0530

    created components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Comments from './comments/Comments';
 
 function App() {
   return (
     <div className="App">
       <h1>comments</h1>
+      <Comments currentUserId=1 />
     </div>
   );
 }
